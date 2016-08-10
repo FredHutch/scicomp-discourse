b9d5b896b4a9f043aab8ee1f51ac57fa094cc2bb
Michael Gutteridge <mrg@fhcrc.org>
2016-08-10 13:35:35 -0700

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,37 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+docker_installation_script 'default_docker' do
+  repo 'main'
+  action :create
+end
+
+directory '/var/discourse' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+bash 'clone_repo' do
+  cwd '/var'
+  code <<-EREH
+    git clone https://github.com/discourse/discourse_docker.git /var/discourse
+  EREH
+  not_if { ::File.exist?('/var/discourse/discourse-setup') }
+end
+
+directory '/var/discourse/containers' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+template '/var/discourse/containers/app.yml' do
+  source 'app.yml.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
